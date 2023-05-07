1a559cc89961a9bf0486b621b976562e06638b06
bene <matheusbenediht@gmail.com>
2023-05-07 13:09:29 -0300

    Adicionando metodo update

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -4,9 +4,47 @@ class UserController {
   async create(req, res) {
     try {
       const novoUser = await User.create(req.body);
-      res.json(novoUser);
+      return res.json(novoUser);
     } catch (e) {
-      res.status(400).json({ errors: e.errors.map((err) => err.message) });
+      return res.status(400).json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
+
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+      return res.json(user);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['ID não enviado'],
+        });
+      }
+      const user = await User.findByPk(req.params.id);
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não encontrado'],
+        });
+      }
+      const dadosAtualizados = await user.update(req.body);
+      return res.json(dadosAtualizados);
+    } catch (e) {
+      return res.json(null);
     }
   }
 }
--- src/routes/user.js
+++ src/routes/user.js
@@ -4,5 +4,7 @@ import userController from '../controllers/User';
 const router = new Router();
 
 router.post('/', userController.create);
-
+router.get('/', userController.index);
+router.get('/:id', userController.show);
+router.put('/:id', userController.update);
 export default router;
