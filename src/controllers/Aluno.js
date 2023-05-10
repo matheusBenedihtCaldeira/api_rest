3bd37360c2e3977381b6754bea4185aff2870ba6
bene <matheusbenediht@gmail.com>
2023-05-10 13:46:34 -0300

    Adicionando CRUDE dos alunos

--- src/controllers/Aluno.js
+++ src/controllers/Aluno.js
@@ -5,6 +5,88 @@ class AlunoController {
     const alunos = await Aluno.findAll();
     res.json(alunos);
   }
+
+  async create(req, res) {
+    try {
+      const aluno = Aluno.create(req.body);
+      return res.json(aluno);
+    } catch (e) {
+      return res.status(400).json({
+        erros: e.erros.map((err) => err.message),
+      });
+    }
+  }
+
+  async shows(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          erros: ['Faltando id'],
+        });
+      }
+      const aluno = await Aluno.findByPk(id);
+      if (!aluno) {
+        return res.status(400).json({
+          erros: ['Aluno não existe'],
+        });
+      }
+      return res.json(aluno);
+    } catch (e) {
+      return res.status(400).json({
+        erros: e.erros.map((err) => err.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          erros: ['Faltando id'],
+        });
+      }
+      const aluno = await Aluno.findByPk(id);
+      if (!aluno) {
+        return res.status(400).json({
+          erros: ['Aluno não existe'],
+        });
+      }
+      await aluno.destroy();
+      return res.json({
+        status: 'success',
+        message: 'Aluno apagado do sistema',
+      });
+    } catch (e) {
+      return res.status(400).json({
+        erros: e.erros.map((err) => err.message),
+      });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          erros: ['Faltando id'],
+        });
+      }
+      const aluno = await Aluno.findByPk(id);
+      if (!aluno) {
+        return res.status(400).json({
+          erros: ['Aluno não existe'],
+        });
+      }
+      const novosDados = aluno.update(req.body);
+      return res.json(novosDados);
+    } catch (e) {
+      return res.status(400).json({
+        erros: e.erros.map((err) => err.message),
+      });
+    }
+  }
 }
 
 export default new AlunoController();
--- src/models/Aluno.js
+++ src/models/Aluno.js
@@ -3,12 +3,62 @@ import Sequelize, { Model } from 'sequelize';
 export default class Aluno extends Model {
   static init(sequelize) {
     super.init({
-      nome: Sequelize.STRING,
-      sobrenome: Sequelize.STRING,
-      email: Sequelize.STRING,
-      idade: Sequelize.INTEGER,
-      peso: Sequelize.FLOAT,
-      altura: Sequelize.FLOAT,
+      nome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [2, 255],
+            msg: 'Nome precisa ter entre 2 e 255 caracteres.',
+          },
+        },
+      },
+      sobrenome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [2, 255],
+            msg: 'Sobrenome precisa ter entre 2 e 255 caracteres.',
+          },
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          isEmail: {
+            msg: 'E-mail invalido',
+          },
+        },
+      },
+      idade: {
+        type: Sequelize.INTEGER,
+        defaultValue: '',
+        validate: {
+          isInt: {
+            msg: 'Idade precisa ser um número inteiro.',
+          },
+        },
+      },
+      peso: {
+        type: Sequelize.FLOAT,
+        defaultValue: '',
+        validate: {
+          isFloat: {
+            msg: 'Peso precisa ser um número inteiro ou de ponto flutuante',
+          },
+        },
+      },
+      altura: {
+        type: Sequelize.FLOAT,
+        defaultValue: '',
+        validate: {
+          isFloat: {
+            msg: 'Altura precisa ser um número inteiro ou de ponto flutuante',
+          },
+        },
+      },
     }, {
       sequelize,
     });
