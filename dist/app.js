1788f9ba133776f137829d87ef247cefdfb3fad1
bene <matheusbenediht@gmail.com>
2023-05-23 23:21:14 -0300

    Retirando delay

--- dist/app.js
+++ dist/app.js
@@ -7,7 +7,6 @@ require('./database');
 var _express = require('express'); var _express2 = _interopRequireDefault(_express);
 var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
 var _helmet = require('helmet'); var _helmet2 = _interopRequireDefault(_helmet);
-var _expressdelay = require('express-delay'); var _expressdelay2 = _interopRequireDefault(_expressdelay);
 var _home = require('./routes/home'); var _home2 = _interopRequireDefault(_home);
 var _user = require('./routes/user'); var _user2 = _interopRequireDefault(_user);
 var _token = require('./routes/token'); var _token2 = _interopRequireDefault(_token);
@@ -41,7 +40,6 @@ class App {
   middlewares() {
     this.app.use(_cors2.default.call(void 0, corsOptions));
     this.app.use(_helmet2.default.call(void 0, ));
-    this.app.use(_expressdelay2.default.call(void 0, 2000));
     this.app.use(_express2.default.urlencoded({ extended: true }));
     this.app.use(_express2.default.json());
     this.app.use(_express2.default.static(_path.resolve.call(void 0, __dirname, 'uploads')));
--- src/app.js
+++ src/app.js
@@ -7,7 +7,6 @@ import './database';
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
-import delay from 'express-delay';
 import home from './routes/home';
 import user from './routes/user';
 import token from './routes/token';
@@ -41,7 +40,6 @@ class App {
   middlewares() {
     this.app.use(cors(corsOptions));
     this.app.use(helmet());
-    this.app.use(delay(2000));
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(express.static(resolve(__dirname, 'uploads')));
